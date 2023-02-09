97525772ba3d65ca153cb00d002f875aacdc01ba
David Kunwar <dkunwar7111@gmail.com>
2023-02-09 21:18:52 +0530

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,34 @@
-import express, { Request, Response } from 'express'
-import { HashTagGenerator } from './HashTagGenrerator';
-import cors from 'cors'
-
+import express, { Request, Response } from "express";
+import { HashTagGenerator } from "./HashTagGenrerator";
+import cors from "cors";
 
 const app = express();
-app.use(express.urlencoded())
-app.use(express.json())
-app.use(cors({ origin: "*", methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
-
-
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) =>  { res.json({ hashtagtypes: ['hastag']})} );
+app.get("/", (req: Request, res: Response) => {
+  res.json({ hashtagtypes: ["hastag"] });
+});
 
 //Route for the hashtag generator
-app.post('/hashtaggenerator', (req: Request, res: Response) =>  { 
-    
-    try {
-        const caption = req.body.caption
+app.post("/hashtaggenerator", (req: Request, res: Response) => {
+  try {
+    const caption = req.body.caption;
 
-        const Result = HashTagGenerator(caption)
+    const Result = HashTagGenerator(caption);
 
-        return res.json({ hashtags: Result })
-
-
-    }
-    catch(e){
-        return res.status(400).send(e)
-    }
-    
+    return res.json({ hashtags: Result });
+  } catch (e) {
+    return res.status(400).send(e);
+  }
 });
 
 app.listen(PORT, () => {
